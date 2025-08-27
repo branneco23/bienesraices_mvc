61ebe508cfd2f766d017f293ecb923837f048ba9
branneco23 <branmeco22@gmail.com>
2025-08-27 13:30:42 -0500

    Instalando tailwindcss

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', './views')
 
+//Carpeta pública
+app.use(express.static('public'))
+
 //Routing
 app.use('/auth', usuarioRoutes)
 
