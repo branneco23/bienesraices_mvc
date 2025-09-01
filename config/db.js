18ba883026e454778fedb00279d2c2accbca4fc8
branneco23 <branmeco22@gmail.com>
2025-09-01 11:37:36 -0500

    Ocultando la información de la base de datos en variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'
+dotenv.config({path: '.env'})
 
-const db = new Sequelize('bienesraices_node_mvc', 'root', 'Aa8ge0050*', {
-    host: 'localhost',
+const db = new Sequelize(process.env.BD_NOMBRE, 
+process.env.BD_USER, process.env.BD_PASS ?? '', {
+    host: process.env.BD_HOST,
     port: '3307',
     dialect: 'mysql',
     define: {
