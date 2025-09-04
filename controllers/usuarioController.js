36ab52a8a560a0ce09e1fb24101590f7fcdcb7c0
branneco23 <branmeco22@gmail.com>
2025-09-04 14:01:30 -0500

    Agregando el resto de Validaciones

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -17,8 +17,13 @@ const registrar = async (req, res) => {
 
     //Validación
     await check('nombre').isEmpty().withMessage('El nombre no puede ir vacio').run(req);
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req);
+    await check('password').isLength({min: 6}).withMessage('El Password debe ser de al menos 6 caracteres').run(req)
+    await check('repitir_password').equals('password').withMessage('Los Passwords no son iguales').run(req)
 
     let resultado = validationResult(req);
+
+    //Verificar que el resultado este vacio
 
     res.json(resultado.array())
 
