482bf107fdc3d325a83ec3213fd1c47cacf24739
branneco23 <branmeco22@gmail.com>
2025-09-12 08:55:33 -0500

    Auto llenado los campos que pasaron la validación

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -17,7 +17,7 @@ const registrar = async (req, res) => {
 
     //Validación
     await check('nombre').notEmpty().withMessage('El nombre no puede ir vacio').run(req);
-   await check('email').isEmail().withMessage('Eso no parece un email').run(req);
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req);
     await check('password').isLength({min: 6}).withMessage('El Password debe ser de al menos 6 caracteres').run(req)
     await check('repitir_password').custom((value, {req}) => value === req.body.password).withMessage('Los Passwords no son iguales').run(req)
 
@@ -28,7 +28,11 @@ const registrar = async (req, res) => {
         //Errores
         return res.render('auth/registro', {
             pagina: 'Crear Cuenta',
-            errores: resultado.array()
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
         })
     }
 
